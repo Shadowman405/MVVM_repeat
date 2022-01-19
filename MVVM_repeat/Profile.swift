d3a94d7e206de2b4a344abab8053fd7684e1480f
Maxim Mitin <maximus405@gmail.com>
2022-01-19 22:26:38 +0300

    creating MVC project

---
+++ MVVM_repeat/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM_repeat
+//
+//  Created by Maxim Mitin on 19.01.22.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
--- MVVM_repeat/ViewController.swift
+++ MVVM_repeat/ViewController.swift
@@ -8,11 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var secondName: UILabel!
+    @IBOutlet weak var age: UILabel!
+    
+    var profile: Profile? {
+        didSet {
+            guard let profile = profile else {return}
+            self.nameLabel.text = profile.name
+            self.secondName.text = profile.secondName
+            self.age.text = "\(profile.age)"
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        profile = Profile(name: "Bogdan", secondName: "Uncle", age: 69)
     }
+    
 
 
 }
